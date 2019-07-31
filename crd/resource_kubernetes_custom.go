a07fb59b8d0595c3da2ff7ddcda7db6245cef639
Chris Alexander <crainte@hybridoptix.net>
2019-07-31 16:45:50 -0500

    Code to read resource

--- crd/resource_kubernetes_custom.go
+++ crd/resource_kubernetes_custom.go
@@ -1,7 +1,14 @@
 package crd
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/hashicorp/terraform/helper/schema"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	Kschema "k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 )
 
 func resourceKubernetesCustom() *schema.Resource {
@@ -17,16 +24,31 @@ func resourceKubernetesCustom() *schema.Resource {
 				Type:        schema.TypeString,
 				Description: "The api group",
 				Required:    true,
+				ForceNew:    true,
 			},
 			"version": {
 				Type:        schema.TypeString,
 				Description: "The api version (ex. v1alpha3)",
 				Required:    true,
+				ForceNew:    true,
 			},
 			"resource": {
 				Type:        schema.TypeString,
 				Description: "The name of the resource",
 				Required:    true,
+				ForceNew:    true,
+			},
+			"namespace": {
+				Type:        schema.TypeString,
+				Description: "The namespace of the resource",
+				Required:    true,
+				ForceNew:    true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "Name of the resource",
+				Required:    true,
+				ForceNew:    true,
 			},
 			"file": {
 				Type:        schema.TypeString,
@@ -42,6 +64,25 @@ func resourceCRDCreate(d *schema.ResourceData, meta interface{}) error {
 }
 
 func resourceCRDRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*dynamic.Clientset)
+
+	group := d.Get("group")
+	version := d.Get("version")
+	resource := d.Get("resource")
+	namespace := d.Get("namespace")
+	name := d.Get("name")
+
+	dynamicResource := Kschema.GroupVersionResource{
+		Group:    group.(string),
+		Version:  version.(string),
+		Resource: resource.(string),
+	}
+
+	dyn, err := conn.Resource(dynamicResource).Namespace(namespace.(string)).Get(name, metav1.GetOptions{})
+	if err != nil {
+		log.Printf("[DEBUG] Received error: %#v", err)
+		return fmt.ErrorF("Failed to read dynamic resource '%s' because: %s", dyn.ObjectMeta, err)
+	}
 	return nil
 }
 
